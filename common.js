a79a3a113c728f51a2b9677907022045fdf61bc4
ricomen <ricomen@mail.ru>
2016-10-06 13:17:25 +0500

    Осваиваем js

---
+++ common.js
@@ -0,0 +1,7 @@
+var number = 234.453;
+console.log( number.toFixed(1) );        
+console.log( number.toExponential() );
+console.log( number.toPrecision(2) );
+console.log( number.toString());
+
+console.log( Math.sqrt( number));
